797f0d6fa363e586a9bf2384b9661c9566beb040
Sam <khsamtran@gmail.com>
2021-10-23 19:15:57 +0100

    added ExpenseDate.css

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -4,7 +4,7 @@ import "./ExpenseItems.css";
 function ExpenseItems(props) {
   return (
     <div className="expense-item">
-      <ExpenseDate />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
