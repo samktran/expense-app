6968b0279b925965b350df18f9da9447ed727bc3
Sam <khsamtran@gmail.com>
2021-10-23 18:59:17 +0100

    added ExpenseDate.js

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,14 +1,16 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItems.css";
 
 function ExpenseItems(props) {
-  return;
-  <div className="expense-item">
-    <div>{props.date.toISOString()}</div>
-    <div className="expense-item__description">
-      <h2>{props.title}</h2>
-      <div className="expense-item__price">£{props.amount}</div>
+  return (
+    <div className="expense-item">
+      <ExpenseDate />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">£{props.amount}</div>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default ExpenseItems;
